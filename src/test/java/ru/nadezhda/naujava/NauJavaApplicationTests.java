ecb2bd1be199c43330eb022ee5c0bdc4b6152e12
hopenz <petrushina_03@inbox.ru>
2024-10-01 15:04:26 +0500

    Initial commit

---
+++ src/test/java/ru/nadezhda/naujava/NauJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nadezhda.naujava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
